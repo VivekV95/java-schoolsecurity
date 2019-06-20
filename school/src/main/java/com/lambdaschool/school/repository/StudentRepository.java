8c126c3cc7769df0ff3431d83e9ed8b19c09cfe3
VivekV95 <vivekv210@gmail.com>
2019-06-20 14:53:06 -0500

    Initial commit- imported school project

---
+++ school/src/main/java/com/lambdaschool/school/repository/StudentRepository.java
@@ -0,0 +1,20 @@
+package com.lambdaschool.school.repository;
+
+import com.lambdaschool.school.model.Student;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface StudentRepository extends PagingAndSortingRepository<Student, Long>
+{
+    List<Student> findByStudnameContainingIgnoreCase(String name, Pageable pageable);
+
+    @Modifying
+    @Query(value = "INSERT INTO studcourses (studid, courseid) VALUES (:studentid, :courseid)", nativeQuery = true)
+    void assignStudentToCourse(long studentid, long courseid);
+}
